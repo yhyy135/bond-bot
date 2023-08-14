b48ae2f8a09af2fe50cbab63a7ae4dcd7c6fc0c5
Acuario <yhyy135@gmail.com>
2023-08-14 14:22:29 +0800

    feat: support multiple chat id

--- sender.go
+++ sender.go
@@ -6,12 +6,15 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func MessageSender(bot *tgbotapi.BotAPI, id int64, message string) {
-	bot.Send(
-		tgbotapi.NewMessage(
-			id, message,
-		),
-	)
+func MessageSender(bot *tgbotapi.BotAPI, ids []int64, message string) {
+
+	for _, id := range ids {
+		bot.Send(
+			tgbotapi.NewMessage(
+				id, message,
+			),
+		)
+	}
 
 	log.Println("Message sent successfully")
 }
